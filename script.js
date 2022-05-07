f9f19d462ae6be6be70fb4bf1fdb704716077586
Gustavo Henrique Lopes Martins <gustavogalo22@hotmail.com>
2022-05-07 15:50:06 -0300

    Incremento de funcionalidade -- Mapa de palavra

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert('Tudo certo até aqui')
+// Inserção de dados
+const message = prompt('Digite uma palavra') // Try edit me
+
+// primeira mensagem
+console.log(message);
+
+//Função de tratamento
+function mapadepalavra(message)
+{ let mapa = {};
+  for (let i = 0; i <message.length; i++) 
+    {
+    let letra1 = message[i];
+    if (mapa[letra1])
+    {
+     mapa[letra1].push(i);
+    }
+    else
+    {
+      mapa[letra1] = [i];
+    }
+  }
+  return mapa;
+}
+let palavranomapa = mapadepalavra (message);
+
+// Laço de desmonstração da solução.
+for (let letra2 in palavranomapa) 
+{   
+    console.log(letra2 + ": " + palavranomapa[letra2]);
+}
+
+
+// dado sem tratamento
+console.log(message);
+
